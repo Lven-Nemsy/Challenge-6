62c2b66a1b2eb367475b2fc915a97a4b6dd4bd28
Nedda Elsayed <nedda_e@yahoo.com>
2023-11-21 21:16:13 -0800

    updates to js, css, and html

--- assets/script.js
+++ assets/script.js
@@ -3,6 +3,7 @@ let cityNameInput = document.querySelector("#cityName");
 let searchBtn = document.querySelector(".searchBtn");
 //let cityBtn = cityNameInput.value
 let cityBtn = document.querySelector("button.cityBtn");
+let history = JSON.parse(localStorage.getItem("history")) || [];
 //  cityNameInput;
 
 // localStorage.setItem
@@ -16,11 +17,36 @@ let cityBtn = document.querySelector("button.cityBtn");
 // 	    cityNameInput.value = savedText;
 // };
 
+function storeLocalStorage(city) {
+	if (!history.includes(city)) {
+		history.push(city);
+		localStorage.setItem("history", JSON.stringify(history));
+		createListItem(city);
+	}
+}
+
 function cityLocation() {
 	let cityName = cityNameInput.value;
 	weather(cityName);
 	forecast(cityName);
 }
+
+function createListItem(city) {
+	var p = document.createElement("button");
+	p.textContent = city;
+	p.addEventListener("click", function () {
+		weather(city);
+		forecast(city);
+	});
+	document.getElementById("searchHistory").appendChild(p);
+}
+
+function runOnLoad(){
+	for (let i = 0; i < history.length; i++) {
+		createListItem(history[i]);
+	}
+}
+runOnLoad()
 
 function forecast(city) {
 	let requestUrlTwo = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&units=imperial&appid=${apiKey}`;
@@ -42,23 +68,25 @@ function forecast(city) {
 				data.list[34],
 			];
 
-			var forecastWeatherContainer =	document.getElementById("forecast");
+			var forecastWeatherContainer = document.getElementById("forecast");
 			forecastWeatherContainer.innerHTML = "";
 
 			for (let i = 0; i < arrayItems.length; i++) {
-
+				var div = document.createElement("div");
 				var cityName = document.createElement("h1");
 				var cityTime = document.createElement("h1");
 				var temp = document.createElement("p");
 				var wind = document.createElement("p");
 				var humid = document.createElement("p");
+				let formatDate = new Date(arrayItems[i].dt * 1000)
 
 				//cityName.textContent = data.name;
-				cityTime.textContent = "At " + arrayItems[i].dt_txt;
+				cityTime.textContent = "At " +formatDate.toLocaleDateString() ;
 				temp.textContent = "Temp: " + arrayItems[i].main.temp;
 				wind.textContent = "Wind: " + arrayItems[i].wind.speed;
 				humid.textContent = "Humidity:" + arrayItems[i].main.humidity;
-				forecastWeatherContainer.append(cityName, cityTime, temp, wind, humid);
+				div.append(cityName, cityTime, temp, wind, humid);
+				forecastWeatherContainer.append(div);
 			}
 		})
 		.catch(function (error) {
@@ -80,16 +108,18 @@ function weather(city) {
 			var currentWeatherContainer = document.getElementById("current");
 
 			currentWeatherContainer.innerHTML = "";
-		
+
 			var cityName = document.createElement("h1");
 			var temp = document.createElement("p");
 			var wind = document.createElement("p");
 			var humid = document.createElement("p");
 
+			storeLocalStorage(data.name);
+	
 			cityName.textContent = data.name;
-			temp.textContent = 'Temp: ' + data.main.temp;
-			wind.textContent = 'Wind: ' + data.wind.speed;
-			humid.textContent = 'Humidity:' + data.main.humidity;
+			temp.textContent = "Temp: " + data.main.temp;
+			wind.textContent = "Wind: " + data.wind.speed;
+			humid.textContent = "Humidity:" + data.main.humidity;
 			currentWeatherContainer.append(cityName, temp, wind, humid);
 		})
 		.catch(function (error) {
